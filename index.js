2f3d86f7715b572a1eb7a7462c069995c32b83db
JoshiRohit6441 <joshi.rohit092003@gmail.com>
2024-02-08 13:38:35 +0530

    rohit commit

--- index.js
+++ index.js
@@ -10,85 +10,11 @@ document.addEventListener("DOMContentLoaded", function () {
     .then((html) => {
       document.getElementById("footer-container").innerHTML = html;
     });
-});
-
-// ---------------------mega menue------------------
-$(document).ready(function () {
-  var megaMenu = $(".transparent_background");
-  var contentRecordLink = $(".hovering_mega_menue");
-  var megaMenuTimer;
-
-  contentRecordLink.on("mouseenter", function () {
-    clearTimeout(megaMenuTimer);
-
-    megaMenu.css("display", "block");
-  });
-
-  function hideMegaMenu() {
-    megaMenu.css("display", "none");
-  }
-
-  $(".nav_main, .transparent_background").on("mouseleave", function (e) {
-    var target = $(e.relatedTarget);
-
-    if (
-      !contentRecordLink.is(target) &&
-      !megaMenu.is(target) &&
-      contentRecordLink.has(target).length === 0 &&
-      megaMenu.has(target).length === 0
-    ) {
-      megaMenuTimer = setTimeout(hideMegaMenu, 100);
-    }
-  });
-
-  $(".nav_main, .transparent_background").on("mousemove", function (e) {
-    clearTimeout(megaMenuTimer);
-
-    var target = $(e.target);
-
-    if (
-      !contentRecordLink.is(target) &&
-      !megaMenu.is(target) &&
-      contentRecordLink.has(target).length === 0 &&
-      megaMenu.has(target).length === 0
-    ) {
-      megaMenuTimer = setTimeout(hideMegaMenu, 100);
-    }
-  });
-
-  megaMenu.on("mouseenter", function () {
-    clearTimeout(megaMenuTimer);
-  });
-
-  megaMenu.on("mouseleave", function () {
-    megaMenuTimer = setTimeout(hideMegaMenu, 100);
-  });
-});
-
-// ------------------------------
-$(document).ready(function () {
-  function adjustLayout() {
-    $(".mega_main_item").each(function () {
-      if ($(this).find(".mega_sub_head").css("display") === "none") {
-        $(this).off("click");
-        $(this).find(".mega_sub_head").hide();
-
-        $(this).click(function () {
-          $(this).find(".mega_sub_head").slideToggle();
-        });
-
-        $(this)
-          .find(".mega_sub_head")
-          .click(function (e) {
-            e.stopPropagation();
-          });
-      }
+  fetch("news_ooption.html")
+    .then((response) => response.text())
+    .then((html) => {
+      document.getElementById("second-head-container").innerHTML = html;
     });
-  }
-  adjustLayout();
-  $(window).resize(function () {
-    adjustLayout();
-  });
 });
 
 // ------------------------------------menu-----------------------------------------------
