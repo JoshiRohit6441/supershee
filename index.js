9f1b506c366dfbaf1124e798f458013802d4be5d
JoshiRohit6441 <joshi.rohit092003@gmail.com>
2024-02-08 16:57:06 +0530

    rohit commit

--- index.js
+++ index.js
@@ -30,3 +30,46 @@ document.addEventListener("DOMContentLoaded", function () {
     }
   });
 });
+// --------------------------------------second-head-section--------------------------------
+document.addEventListener("DOMContentLoaded", function () {
+  var prefix = ["first", "second", "third", "fourth", "fifth"];
+  var divs = prefix.map((item) =>
+    document.querySelector("." + item + "_opt_resp")
+  );
+
+  prefix.forEach(function (item, index) {
+    var opts = document.querySelectorAll("." + item + "_opt");
+    if (opts.length > 0) {
+      opts.forEach(function (opt) {
+        opt.addEventListener("click", function (event) {
+          event.preventDefault();
+          console.log("Clicked on " + item + " option");
+          prefix.forEach(function (item, i) {
+            var currDiv = document.querySelector("." + item + "_opt_resp");
+            if (i !== index) {
+              currDiv.classList.add("d-none");
+            } else {
+              currDiv.classList.toggle("d-none");
+            }
+          });
+        });
+      });
+    } else {
+      console.error("Element not found for " + item + " option. Check IDs.");
+    }
+  });
+
+  document.addEventListener("click", function (event) {
+    var isClickedInsideOpt = prefix.some((item) => {
+      var buttons = document.querySelectorAll("." + item + "_opt");
+      return Array.from(buttons).some(
+        (button) =>
+          button.contains(event.target) ||
+          event.target.classList.contains(item + "_opt")
+      );
+    });
+    if (!isClickedInsideOpt) {
+      divs.forEach((div) => div.classList.add("d-none"));
+    }
+  });
+});
